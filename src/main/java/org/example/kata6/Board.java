5f61efa0b6e24a667c58b052785adda4a7895dfc
Willy <thewillyeb@gmail.com>
2024-06-28 14:22:19 +0100

    Board creation done.

--- src/main/java/org/example/kata6/Board.java
+++ src/main/java/org/example/kata6/Board.java
@@ -3,4 +3,17 @@ package org.example.kata6;
 
 public class Board {
 
+    private final String[] state;
+
+    public Board(String state) {
+        this.state = state.split("\n");
+    }
+
+    public String state() {
+        return String.join("\n", this.state);
+    }
+
+    public Board next() {
+        return new Board(".");
+    }
 }
--- src/test/java/tests/BoardTests.java
+++ src/test/java/tests/BoardTests.java
@@ -7,6 +7,24 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class BoardTests {
 
+    @Test
+    public void should_return_empty_board_given_empty_board(){
+        Board board = new Board("");
+        assertEquals(board.state(), "");
+    }
+
+    @Test
+    public void should_return_1x1_board_with_dead_cell_given_1x1_board_with_dead_cell(){
+        Board board = new Board(".");
+        assertEquals(board.state(), ".");
+    }
+
+    @Test
+    public void should_return_1x1_board_with_dead_cell_given_1x1_board_with_alive_cell(){
+        Board board = new Board("X");
+        Board nextBoard = board.next();
+        assertEquals(nextBoard.state(), ".");
+    }
 
 
 }
